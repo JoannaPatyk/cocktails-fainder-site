5033eeb0bafabed56b9722c6a9c120abe5aebc64
Joanna Patyk <patykjoanna93@gmail.com>
2023-07-12 16:07:21 +0200

    First version Added basic features and first look

---
+++ src/presentDrinks.js
@@ -0,0 +1,10 @@
+import fetchDrinks from "./fetchDrinks.js";
+import displayDrinks from "./displayDrinks.js";
+
+const showDrink = async (url) => {
+  const data = await fetchDrinks(url);
+
+  const section = await displayDrinks(data);
+};
+
+export default showDrink;
