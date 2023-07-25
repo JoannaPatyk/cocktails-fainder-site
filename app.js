a3e85588323b50a8eb1fbcc8dd59cceea8e4fd22
Joanna Patyk <patykjoanna93@gmail.com>
2023-07-25 16:42:38 +0200

    Changes in scrolling functionality

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ import './src/searchForm.js';
 
 const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a';
 
+window.addEventListener('DOMContentLoaded', () => {
+    presentDrinks(URL);
+});
+
+// Functionality for scrolling to the selected menu item
+
 document.querySelectorAll('.menu a').forEach(function (element) {
     element.addEventListener('click', () => {
         const goToSectionElement = getElement(`#${element.getAttribute('class')}`);
@@ -16,15 +22,28 @@ document.querySelectorAll('.menu a').forEach(function (element) {
     });
 });
 
+// Functionality for scrolling to the top of the page
+
 const element = getElement('.scroll-icon');
+
+window.addEventListener('scroll', () => {
+    scrollFunction();
+});
+
+function scrollFunction() {
+    const scrollPosition = window.pageYOffset;
+    element.style.display = scrollPosition > 100 ? 'block' : 'none';
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
 element.addEventListener('click', () => {
     window.scrollTo({
         top: getElement('.section-logo').getBoundingClientRect().top,
         behavior: 'smooth'
     });
-});
-
-window.addEventListener('DOMContentLoaded', () => {
-    presentDrinks(URL);
 });
